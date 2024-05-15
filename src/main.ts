abbeb037ce013ae06bd11dba855d372f2cfd292b
scarecrow-11 <hafizurmilon.11@gmail.com>
2024-05-16 01:50:36 +0600

    [chore] use server port from environment variable

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
+const PORT = process.env.PORT || 4000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(4000)
+  await app.listen(PORT)
 }
 bootstrap()
